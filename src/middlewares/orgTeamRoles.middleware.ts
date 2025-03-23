b72bf989b4fad6356d46f58ecc7854150b652755
kolexstrings <koladeolukoya@gmail.com>
2025-03-23 14:42:30 +0100

    fix: teams middleware

--- src/middlewares/orgTeamRoles.middleware.ts
+++ src/middlewares/orgTeamRoles.middleware.ts
@@ -1,16 +1,20 @@
-import { NextFunction, Request, Response, RequestHandler } from "express";
-import HttpException from "../exceptions/http.exception";
+import { NextFunction, Response, RequestHandler } from "express";
 import { Types } from "mongoose";
+import HttpException from "../exceptions/http.exception";
+import teamModel from "../resources/organization/team/team.model";
+import { OrgRole } from "../resources/organization/organization.protocol";
+import { RequestWithUser } from "../types/express";
 
 function verifyTeamRolesMiddleware(allowedRoles: string[]): RequestHandler {
-  return async (req: Request, res: Response, next: NextFunction) => {
+  return async (req: RequestWithUser, res: Response, next: NextFunction) => {
     try {
       if (!req.user) {
         throw new HttpException(401, "error", "Unauthorized");
       }
 
-      const userTeams = req.user.teams;
-      const userOrgRole = req.user.organizationRole;
+      const userId = new Types.ObjectId(String(req.user._id));
+
+      const userOrganizations = req.user.organizations || [];
 
       // Extract teamId from request
       const { teamId } = req.params || req.body;
@@ -19,20 +23,32 @@ function verifyTeamRolesMiddleware(allowedRoles: string[]): RequestHandler {
       }
 
       // **Allow Organization Admins to bypass team role checks**
-      if (userOrgRole === "Admin") {
+      const isOrgAdmin = userOrganizations.some(
+        (org) => org.role === OrgRole.Admin
+      );
+
+      if (isOrgAdmin) {
         return next();
       }
 
-      // Find user's role in the team
-      const userTeam = userTeams.find((team) =>
-        new Types.ObjectId(team.teamId).equals(new Types.ObjectId(teamId))
+      // **Fetch the Team document**
+      const team = await teamModel.findById(teamId);
+
+      if (!team) {
+        throw new HttpException(404, "error", "Team not found.");
+      }
+
+      // **Check if the user is a member of the team**
+      const userTeamMember = team.members.find((member) =>
+        member.memberId.equals(userId)
       );
 
-      if (!userTeam) {
+      if (!userTeamMember) {
         throw new HttpException(403, "error", "User is not part of this team.");
       }
 
-      if (!allowedRoles.includes(userTeam.role)) {
+      // **Check if the user's role is allowed**
+      if (!allowedRoles.includes(userTeamMember.role)) {
         throw new HttpException(
           403,
           "error",
--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,7 +1,24 @@
 import { User } from "../resources/user/user.protocol";
+import { Request } from "express";
 
 declare module "express-serve-static-core" {
   interface Request {
     user?: any;
   }
+}
+
+export interface User {
+  id: string;
+  organizations: {
+    orgId: string;
+    role: string;
+    teams?: {
+      teamId: string;
+      role: string;
+    }[];
+  }[];
+}
+
+export interface RequestWithUser extends Request {
+  user?: User;
 }
