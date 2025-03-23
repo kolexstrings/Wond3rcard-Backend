ae2c0b0999b8553f16530ffda78f267c0409ec96
kolexstrings <koladeolukoya@gmail.com>
2025-03-23 09:30:07 +0100

    build: fixed build error

--- src/utils/has-team-role.ts
+++ src/utils/has-team-role.ts
@@ -1,15 +1,15 @@
-import { TeamRole } from "../resources/organization/organization.protocol";
+import { OrgRole } from "../resources/organization/organization.protocol";
 import { User } from "../resources/user/user.protocol";
 import { Types } from "mongoose";
 
 function hasTeamRole(
   user: User,
   organizationId: string,
-  role: TeamRole
+  role: OrgRole
 ): boolean {
   return user.organizations.some(
     (orgRole) =>
-      new Types.ObjectId(orgRole.organizationId).equals(
+      new Types.ObjectId(orgRole.memberId).equals(
         new Types.ObjectId(organizationId)
       ) && orgRole.role === role
   );
